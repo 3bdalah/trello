c54526506817b6d4d62d9e372868ae1fcbd33f16
3bdalah <3bdallah.dev@gmail.com>
2023-10-17 17:42:27 +0300

    feat(sidebar.jsx): built sidebar and added  some routes - added some routes [setting , Help, Notifications]

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import SideBar from "../SideBar/SideBar";
 
 export default function Layout() {
   return (
     <>
       <div className=" bg-stone-50">
         <Header />
+        <SideBar />
         <Outlet />
         <Footer />
       </div>
