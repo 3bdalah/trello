c8f4cc0eb196d38888ffe90f69da35dd31b047e5
3bdalah <3bdallah.dev@gmail.com>
2023-10-19 17:34:30 +0300

    feat(create-task.jsx):built featture add task to another employee

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../Footer/Footer";
+// import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import SideBar from "../SideBar/SideBar";
 
@@ -10,7 +10,7 @@ export default function Layout() {
         <Header />
         <SideBar />
         <Outlet />
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </>
   );
