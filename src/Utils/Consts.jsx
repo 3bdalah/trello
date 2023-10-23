1c4733faab154e9546eb76b159bbe4eddff6889b
3bdalah <3bdallah.dev@gmail.com>
2023-10-23 22:21:39 +0300

    style(sidebar): complete style sidebar and update last style guide

--- src/Utils/Consts.jsx
+++ src/Utils/Consts.jsx
@@ -1,8 +1,36 @@
 const links = [
   {
+    to: "/help",
+    text: "help",
+    icone: <i className="fa fa-question"></i>,
+  },
+];
+
+export const linksOverview = [
+  {
     to: "/",
     text: "home",
     icone: <i className="fa fa-home"></i>,
+  },
+  {
+    to: "/dashboard",
+    text: "dashboard",
+    icone: <i className="fa fa-dashboard"></i>,
+  },
+  {
+    to: "/about",
+    text: "about",
+    icone: <i className="fa fa-book"></i>,
+  },
+];
+
+export const linksAdmin = [
+  { to: "/profile", text: "profile", icone: <i className="fa fa-user"></i> },
+
+  {
+    to: "/notifications",
+    text: "notification",
+    icone: <i className="fa fa-bell"></i>,
   },
   {
     to: "/created-tasks",
@@ -10,30 +38,14 @@ const links = [
     icone: <i className="fa fa-calendar-check"></i>,
   },
   {
-    to: "/add-task",
-    text: "add-task",
-    icone: <i className="fa fa-plus"></i>,
-  },
-  {
-    to: "/chats",
-    text: "chats",
-    icone: <i className="fa fa-comments"></i>,
-  },
-  {
-    to: "/notifications",
-    text: "notification",
-    icone: <i className="fa fa-bell"></i>,
-  },
-  {
-    to: "/dashboard",
-    text: "dashboard",
-    icone: <i className="fa fa-dashboard"></i>,
-  },
-  { to: "/profile", text: "profile", icone: <i className="fa fa-user"></i> },
-  {
     to: "/my-tasks",
     text: "My-Tasks",
     icone: <i className="fas fa-check fa-solid"></i>,
+  },
+  {
+    to: "/add-task",
+    text: "add-task",
+    icone: <i className="fa fa-plus"></i>,
   },
   {
     to: "/employees",
@@ -41,20 +53,9 @@ const links = [
     icone: <i className="fa fa-users"></i>,
   },
   {
-    to: "/setting",
-    text: "setting",
-    icone: <i className="fa fa-gear"></i>,
-  },
-
-  {
-    to: "/logout",
-    text: "logout",
-    icone: <i className="fa fa-face-sad-tear text-red-800"></i>,
-  },
-  {
-    to: "/help",
-    text: "help",
-    icone: <i className="fa fa-question"></i>,
+    to: "/chats",
+    text: "chats",
+    icone: <i className="fa fa-comments"></i>,
   },
 ];
 
