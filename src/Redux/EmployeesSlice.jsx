60226245017dedd1d617bff1dcdf6006645624bf
3bdalah <3bdallah.dev@gmail.com>
2023-10-27 22:33:16 +0200

    feat(dashboard): built component dashboard and create employeee tasks

---
+++ src/Redux/EmployeesSlice.jsx
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export let handleGetAllEmployee = createAsyncThunk(
+  "employees/handleGetAllEmployee",
+  async () => {
+    try {
+      let { data } = await axios.get(
+        "https://trello-backend-tlg1.onrender.com/getAllUsers",
+        {
+          headers: {
+            token: localStorage.getItem("token"),
+          },
+        }
+      );
+      return data.users;
+    } catch (error) {
+      console.log("errors get all users", error);
+    }
+  }
+);
+
+let initialState = {
+  allEmployees: [],
+  counterEmployees: 0,
+};
+let employeesSlice = createSlice({
+  name: "employees",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(handleGetAllEmployee.fulfilled, (state, action) => {
+      state.allEmployees = action.payload;
+      state.counterEmployees = state.allEmployees.length;
+    });
+  },
+});
+
+export let employeesReducer = employeesSlice.reducer;
