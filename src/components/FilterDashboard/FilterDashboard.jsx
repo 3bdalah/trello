054f6e93af00d68a232df1cdccb6a987cd701a08
3bdalah <3bdallah.dev@gmail.com>
2023-11-10 23:52:21 +0200

    feat(filterdashboard) : feature filter still in progress

--- src/components/FilterDashboard/FilterDashboard.jsx
+++ src/components/FilterDashboard/FilterDashboard.jsx
@@ -2,22 +2,21 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 export default function FilterDashboard() {
   let validationSchema = Yup.object({
-    email: Yup.string().email("Invalid email").required("Email is required"),
-    password: Yup.string().matches(
-      /^(?=.*[A-Z])(?=.*\d).{6,}$/,
-      "Invalid password. Password must have at least one uppercase letter, one digit, and be at least 6 characters long."
-    ),
+    queryFilter: Yup.string(),
+    statusTask: Yup.string(),
   });
 
   let initialValuesFilter = {
     queryFilter: "",
     statusTask: "",
   };
+
   let formikFilter = useFormik({
-    initialValuesFilter,
-    validationSchema,
+    initialValues: initialValuesFilter,
+    validationSchema: validationSchema,
     onSubmit: () => {},
   });
+
   return (
     <>
       <div className="bg-white border border-gray-200 p-2 m-2 h-fit shadow-sm rounded-md pb-4">
@@ -30,7 +29,7 @@ export default function FilterDashboard() {
             >
               <div className="group-input  w-1/3  flex flex-col items-start content-start m-2">
                 <label className="text-gray-500 capitalize text-left mb-2">
-                  search :
+                  search
                 </label>
                 <input
                   className="h-10 bg-slate-100 border p-2 rounded-md w-full border-gray-400"
@@ -42,12 +41,17 @@ export default function FilterDashboard() {
               </div>
               <div className="group-input  w-1/3  flex flex-col items-start content-start m-2">
                 <label className="text-gray-500 capitalize text-left mb-2">
-                  status :
+                  status
                 </label>
-                <select className="h-10 bg-slate-100 border p-2 cursor-pointer rounded-md w-full border-gray-400">
+                <select
+                  className="h-10 bg-slate-100 border p-2 cursor-pointer rounded-md w-full border-gray-400"
+                  name="statusTask"
+                  value={formikFilter.values.statusTask}
+                  onChange={formikFilter.handleChange}
+                >
                   <option value={"all"}>All</option>
                   <option value={"toDo"}>todo</option>
-                  <option value={"doning"}>doning</option>
+                  <option value={"doing"}>doing</option>
                   <option value={"done"}>done</option>
                 </select>
               </div>
@@ -55,7 +59,7 @@ export default function FilterDashboard() {
                 {/* <label className="text-gray-500 capitalize text-left mb-2">
                   clear :
                 </label> */}
-                <button className="h-10 font-semibold bg-red-100 text-red-800 cursor-pointer hover:shadow-md duration-300  transition border p-2 rounded-md w-full border-gray-400">
+                <button className="hover:bg-red-700 hover:text-white h-10 font-semibold bg-red-100 text-red-800 cursor-pointer hover:shadow-md duration-300  transition border p-2 rounded-md w-full border-gray-400">
                   Clear Filter
                 </button>
               </div>
